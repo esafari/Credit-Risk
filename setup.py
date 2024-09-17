c13bbea9077ca37420c7c62335c8a36367b8cdae
e.safari <ehram.safari@gmail.com>
2024-09-17 00:24:25 -0400

    creatin setup files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+nqme='Credit Risk',
+version='0.0.1',
+author='Ehram Safari',
+author_email='Ehram.safari@gmail.com',
+packages=find_packages(),
+install_requiers=['pandas','numpy','seaborn'],
+#install_requiers=
+)
